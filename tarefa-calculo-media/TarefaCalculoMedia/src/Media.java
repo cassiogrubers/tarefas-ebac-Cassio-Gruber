59ed25c408ddcef84679ced5af45c1777f0d1bfa
cassiogrubers <cassiogruber.s@gmail.com>
2023-11-30 03:24:15 -0400

    tarefa calculo media

---
+++ tarefa-calculo-media/TarefaCalculoMedia/src/Media.java
@@ -0,0 +1,13 @@
+public class Media {
+    public static void main(String args[]){
+        int nota1, nota2, nota3, nota4, media;
+
+        nota1= 8;
+        nota2= 7;
+        nota3 = 9;
+        nota4 = 10;
+
+        media = (nota1+nota2+nota3+nota4)/4;
+        System.out.println("A media do aluno foi "+ media);
+    }
+}
