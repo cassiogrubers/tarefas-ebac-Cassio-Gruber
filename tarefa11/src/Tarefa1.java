f83aee740e67b35f62b7c238f4f5c6ce5a3f271f
cassiogrubers <102683761+cassiogrubers@users.noreply.github.com>
2024-03-14 17:30:06 -0400

    Update Tarefa1.java

--- tarefa11/src/Tarefa1.java
+++ tarefa11/src/Tarefa1.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class Tarefa1 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Digite os nomes separados por ,");
         List<String> lista = new ArrayList<>();
         String linha = scanner.nextLine();
         String []itens = linha.split(",");
